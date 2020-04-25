6be1d664cf6ad6478f2d2226b6971550b441bb13
Tolulope Idowu <tidowu@redhat.com>
2020-04-25 17:41:50 -0400

    added rollup config

--- GruntFile.js
+++ GruntFile.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const WrapperPlugin = require("wrapper-webpack-plugin");
+const resolve = require("@rollup/plugin-node-resolve");
+const { terser } = require("rollup-plugin-terser");
 
 module.exports = grunt => {
   grunt.initConfig({
@@ -12,7 +14,7 @@ module.exports = grunt => {
         },
         output: {
           path: path.resolve(__dirname, "dist"),
-          filename: "[name].js",
+          filename: "[name]-webpack.js",
           libraryTarget: "umd"
         },
         plugins: [
@@ -25,8 +27,23 @@ module.exports = grunt => {
           })
         ]
       }
+    },
+    // Doesn't work exactly like webpack (have to store PFE elements under pfe namespace).
+    // TODO: Figure out a backdoor trick.
+    rollup: {
+      pfe: {
+        options: {
+          plugins: [resolve(), terser()],
+          format: "umd",
+          name: "pfe"
+        },
+        files: {
+          "./dist/pfe-rollup.js": "./src/pfe.js"
+        }
+      }
     }
   });
   grunt.loadNpmTasks("grunt-webpack");
-  grunt.registerTask("default", ["webpack"]);
+  grunt.loadNpmTasks("grunt-rollup");
+  grunt.registerTask("default", ["webpack", "rollup"]);
 };
